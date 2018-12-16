327f4922aac078326e2c57cc66b395090e090eee
litterel <litterel@hotmail.com>
2018-12-16 21:33:11 +0800

    add template

--- hello.py
+++ hello.py
@@ -1,16 +1,38 @@
-from flask import Flask, url_for,render_template
+from flask import (Flask, make_response, redirect, render_template, request,
+                   url_for)
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class my_form(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    # submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pku_phy'
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    response = make_response('<h1>Index page</h1>')
+    return response
+
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = my_form()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
+
 
 @app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html',name=name)
+    return render_template('hello.html', name=name)
 
 
 @app.route('/user/<username>')
@@ -31,6 +53,7 @@ def show_subpath(subpath):
 @app.route('/login')
 def login():
     return 'login...'
+
 
 '''
 with app.test_request_context():
